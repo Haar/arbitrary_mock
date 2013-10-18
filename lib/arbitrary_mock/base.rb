19936044c1fbc92eef5053f1e49b8fbc14621129
Robert White <robert@terracoding.com>
2013-10-18 08:31:59 +0100

    Fix missing namespace issues when using only base

--- lib/arbitrary_mock/base.rb
+++ lib/arbitrary_mock/base.rb
@@ -1,16 +1,18 @@
-class ArbitraryMock::Base
+module ArbitraryMock
+  class Base
 
-  def initialize(property_hash={})
-    @property_hash = property_hash
-  end
+    def initialize(property_hash={})
+      @property_hash = property_hash
+    end
 
-  def method_missing(*args)
-    method_name = args.first
-    setter_method_match = /(.*)=$/.match(method_name)
+    def method_missing(*args)
+      method_name = args.first
+      setter_method_match = /(.*)=$/.match(method_name)
 
-    return @property_hash[method_name] if setter_method_match.nil?
+      return @property_hash[method_name] if setter_method_match.nil?
 
-    setter_method = setter_method_match.captures.first.to_sym
-    @property_hash[setter_method] = args[1]
+      setter_method = setter_method_match.captures.first.to_sym
+      @property_hash[setter_method] = args[1]
+    end
   end
 end
